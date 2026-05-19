86010d9b4e97b075d1e473e5eba6437f5dc18371
Thomas Høy Bowman <thomas.bowman.2003@gmail.com>
2026-05-19 18:23:05 +0200

    new becnhmark

--- test/benchmarks/benchmark.js
+++ test/benchmarks/benchmark.js
@@ -27,62 +27,39 @@ var program = "* Sender\n" +
     "* Medium\n" +
     "agent Med = MedTop | MedBot;\n" +
     "agent MedBot = left0.MedBotRep0 + left1.MedBotRep1;\n" +
-    "agent MedBotRep0 = 'right0.MedBotRep0 + tau.MedBot;\n" +
-    "agent MedBotRep1 = 'right1.MedBotRep1 + tau.MedBot;\n" +
+    "agent MedBotRep0 = 'right0.MedBotRep0 + MedBot;\n" + //Removed tau
+    "agent MedBotRep1 = 'right1.MedBotRep1 + MedBot;\n" + //Removed tau
     "agent MedTop = rightAck0.MedTopRep0 + rightAck1.MedTopRep1;\n" +
-    "agent MedTopRep0 = 'leftAck0.MedTopRep0 + tau.MedTop;\n" +
-    "agent MedTopRep1 = 'leftAck1.MedTopRep1 + tau.MedTop;\n" +
-    "\n" +
-    "* Sender\n" +
-    "agent ZSend0 = acc.ZSending0;\n" +
-    "agent ZSending0 = 'left0.ZSending0 + leftAck0.ZSend1 + leftAck1.ZSending0;\n" +
-    "agent ZSend1 = acc.ZSending1;\n" +
-    "agent ZSending1 = 'left1.ZSending1 + leftAck1.ZSend0 + leftAck0.ZSending1;\n" +
-    "\n" +
-    "* Receiver\n" +
-    "agent ZReceived0 = 'del.ZRecvAck1;\n" +
-    "agent ZReceived1 = 'del.ZRecvAck0;\n" +
-    "agent ZRecvAck0 = right0.ZReceived0 + right1.ZRecvAck0 + 'rightAck1.ZRecvAck0;\n" +
-    "agent ZRecvAck1 = right1.ZReceived1 + right0.ZRecvAck1 + 'rightAck0.ZRecvAck1;\n" +
-    "\n" +
-    "* Medium\n" +
-    "agent ZMed = ZMedTop | ZMedBot;\n" +
-    "agent ZMedBot = left0.ZMedBotRep0 + left1.ZMedBotRep1;\n" +
-    "agent ZMedBotRep0 = 'right0.ZMedBotRep0 + tau.ZMedBot;\n" +
-    "agent ZMedBotRep1 = 'right1.ZMedBotRep1 + tau.ZMedBot;\n" +
-    "agent ZMedTop = rightAck0.ZMedTopRep0 + rightAck1.ZMedTopRep1;\n" +
-    "agent ZMedTopRep0 = 'leftAck0.ZMedTopRep0 + tau.ZMedTop;\n" +
-    "agent ZMedTopRep1 = 'leftAck1.ZMedTopRep1 + tau.ZMedTop;\n" +
+    "agent MedTopRep0 = 'leftAck0.MedTopRep0 + MedTop;\n" + //Removed tau
+    "agent MedTopRep1 = 'leftAck1.MedTopRep1 + MedTop;\n" + //Removed tau
     "\n" +
     "* Protocol and specification\n" +
     "set InternalComActs = {left0, left1, right0, right1, leftAck0, leftAck1, rightAck0, rightAck1};\n" +
     "agent Protocol = (Send0 | Med | RecvAck0) \\ InternalComActs;\n" +
-    "agent ZProtocol = (ZSend0 | ZMed | ZRecvAck0) \\ InternalComActs;\n";
+    "agent Spec = acc.'del.Spec;";
 
 function bisimulation(graph) {
     var succGen = getStrictSuccGenerator(graph),
+        defendSuccGen = getWeakSuccGenerator(graph),
         protocol = graph.processByName("Protocol").id,
-        spec = graph.processByName("ZProtocol").id;
-    Equivalence.isBisimilar(succGen, succGen, protocol, spec, graph);
+        spec = graph.processByName("Spec").id;
+    isBisimilar = Equivalence.isBisimilar(succGen, defendSuccGen, protocol, spec, graph);
 };
 
 var benchParsing = new Benchmark("Parsing", function () {
     CCSParser.parse(program, {ccs: ccs});
 });
 
-var benchBisim = new Benchmark(
-    "Strong Bisimulation",
-    function () {
-        bisimulation(graph);
+var benchBisim = new Benchmark("Strong Bisimulation", function () {
+    bisimulation(graph);
+}, {
+    'setup': function () {
+        var graph = CCSParser.parse(program, {ccs: ccs});
     },
-    {
-        'setup': function () {
-            var graph = CCSParser.parse(program, {ccs: ccs});
-        },
-        minSamples: 100,
-        maxTime: 0
-    }
-);
+    minSamples: 100,
+    maxTime: 0
+});
+
 var suite = new Benchmark.Suite();
 
 suite
